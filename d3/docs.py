9c5fc6085b3501b4e60f91882d2fb603f1ebf417
MikiKru <michal_kruczkowski@o2.pl>
2021-11-24 14:40:28 +0100

    ex 4_5

---
+++ d3/docs.py
@@ -0,0 +1,14 @@
+def sum(num1, num2):
+    """
+    Returns the sum of multiple numerical arguments.
+
+    The arguments are zero or more numbers.
+    The result is their sum.
+
+    :param num1: xxx
+    :param num2: xxx
+    :return: xxx
+    """
+    return num1 + num2
+
+print(sum(1,2))
